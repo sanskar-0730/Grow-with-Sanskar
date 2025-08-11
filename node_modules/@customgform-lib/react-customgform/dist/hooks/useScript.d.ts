d25583fde9ec5d9e67a7dfc9b67f5289bb558d25
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-08-11 20:14:23 +0530

    Add .gitignore and remove node_modules from tracking

--- node_modules/@customgform-lib/react-customgform/dist/hooks/useScript.d.ts
+++
@@ -1,6 +0,0 @@
-export type UseScriptStatus = 'idle' | 'loading' | 'ready' | 'error';
-export interface UseScriptOptions {
-    shouldPreventLoad?: boolean;
-    removeOnUnmount?: boolean;
-}
-export declare function useScript(src: string | null, options?: UseScriptOptions): UseScriptStatus;
