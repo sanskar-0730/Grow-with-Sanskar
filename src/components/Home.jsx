6961a58743614566ab39bb58f9a36e0948519fbf
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-08-12 14:57:14 +0530

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import Footer from './Footer'
+import my_image from '/my_image.png'
 
 const Home = () => {
+
+  const fullText = " Hi, My Name is Sanskar Gupta.";
+
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (index < fullText.length) {
+        setDisplayedText((prev) => prev + fullText[index]);
+        setIndex((prev) => prev + 1);
+      }
+      else {
+        clearInterval(interval);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
   return (
     <div className='home'>
       
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
-      <div>This is home</div>
+      <div className='container'>
+        <div className='my_name'>
+          <b>{displayedText}</b>
+          <span className="cursor">|</span>
+        </div>
+        <div className='my_image'>
+          <img src={my_image} alt="sanskar" />
+        </div>
+      </div>
+
+      <div className="section">
+        <b>MORE ADDING SOON....</b>
+      </div>
 
     </div>
   )
